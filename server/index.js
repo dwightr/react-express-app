01002136bdce65ddc631c466c972a588aa78cfad
Dwight Ramokhola <dwight.ramokhola@Dwight-MacBook-Pro.local>
2018-08-23 22:32:15 +0200

    initial commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const port = 4000
+
+const express = require('express')
+const compression = require('compression')
+
+const app = express()
+
+app.use(compression())
+app.use(express.static('./build'))
+
+app.get('/', (req, res) => res.sendFile('./build/index.html'))
+
+app.listen(port, '0.0.0.0', () => {
+    console.log(`listening on http://0.0.0.0:${port}`)
+})
